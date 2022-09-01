4ad7d041425961de77196af3b986a8773a0c9554
“doniwirawan” <doniwirawan166@gmail.com>
2022-09-01 23:03:41 +0800

    adding middleware

--- Routers/Article.router.js
+++ Routers/Article.router.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../Middleware/Auth.middleware');
+
 const articleController = require('../Controllers/Article.controller')
 
 router.get('/articles', articleController.getArticles)
-router.post('/articles', articleController.postArticles)
+router.post('/articles', auth, articleController.postArticles)
 
 module.exports = router
--- Routers/User.router.js
+++ Routers/User.router.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../Middleware/Auth.middleware');
 const userController = require('../Controllers/User.controller')
 
 
